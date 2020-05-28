1c0230b45958fbee8125f807dc3d2019f4238157
sunad.raj <sunad.raj@lifetrenz.com>
2020-05-28 12:36:37 +0530

    start activity

--- app/src/main/java/com/example/sunadchatapp/RegisterActivity.java
+++ app/src/main/java/com/example/sunadchatapp/RegisterActivity.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class StartActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_register);
     }
 }
