95748fb2ae64c097e60d4325ff9bc41e167dfc5a
sunad.raj <sunad.raj@lifetrenz.com>
2020-05-30 15:16:19 +0530

    login user

--- app/src/main/java/com/example/sunadchatapp/LoginActivity.java
+++ app/src/main/java/com/example/sunadchatapp/LoginActivity.java
@@ -1,4 +1,70 @@
 package com.example.sunadchatapp;
 
-public class LoginActivity {
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    MaterialEditText email, password;
+    Button login;
+
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.Email);
+        password = findViewById(R.id.Password);
+        login = findViewById(R.id.login);
+
+        auth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String text_email = email.getText().toString();
+                String text_password = password.getText().toString();
+
+                if (TextUtils.isEmpty(text_email) || TextUtils.isEmpty(text_email)) {
+                    Toast.makeText(LoginActivity.this, "you cant register this email or password", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    auth.signInWithEmailAndPassword(text_email, text_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+
+                            } else {
+                                Toast.makeText(LoginActivity.this, "authentication failed", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/example/sunadchatapp/RegisterActivity.java
+++ app/src/main/java/com/example/sunadchatapp/RegisterActivity.java
@@ -1,28 +1,47 @@
 package com.example.sunadchatapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+import java.util.HashMap;
+
 public class RegisterActivity extends AppCompatActivity {
+
 
     MaterialEditText username, email, password;
     Button register;
 
     FirebaseUser firebaseUser;
     FirebaseAuth auth;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        /*Toolbar toolbar=findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Register");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+*/
         username = findViewById(R.id.Username);
         email = findViewById(R.id.Email);
         password = findViewById(R.id.Password);
@@ -38,6 +57,47 @@ public class RegisterActivity extends AppCompatActivity {
                 String text_email = email.getText().toString();
                 String text_password = password.getText().toString();
 
+                if (TextUtils.isEmpty(text_name) || TextUtils.isEmpty(text_email) || TextUtils.isEmpty(text_password)) {
+                    Toast.makeText(RegisterActivity.this, "All fields are required", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    register(text_name, text_email, text_password);
+                }
+
+            }
+
+            private void register(final String text_name, String text_email, String text_password) {
+
+                auth.createUserWithEmailAndPassword(text_email, text_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String userid = firebaseUser.getUid();
+                            reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+                            HashMap<String, String> hashMap = new HashMap<>();
+
+                            hashMap.put("id", userid);
+                            hashMap.put("username", text_name);
+                            hashMap.put("imageURL", "default");
+                            reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "you cant register this email or password", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
             }
         });
 
