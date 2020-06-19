0fc7bdb4a56639842e14bbdb8efb950ad165fbab
sunad raj <40906488+sunadraj@users.noreply.github.com>
2020-06-19 15:45:39 +0530

    for better performance small adjustment in code

--- app/src/main/java/com/example/sunadchatapp/MessageActivity.java
+++ app/src/main/java/com/example/sunadchatapp/MessageActivity.java
@@ -130,6 +130,7 @@ public class MessageActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+
             }
         });
 
