dee28c2d51e4e2f0771e8e2806f4780d291b393e
sunad raj <40906488+sunadraj@users.noreply.github.com>
2020-06-25 15:54:23 +0530

    token in chat fragment

--- app/src/main/java/com/example/sunadchatapp/Fragment/ChatsFragment.java
+++ app/src/main/java/com/example/sunadchatapp/Fragment/ChatsFragment.java
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.sunadchatapp.Adapter.UserAdapter;
 import com.example.sunadchatapp.Model.Chatlist;
 import com.example.sunadchatapp.Model.User;
+import com.example.sunadchatapp.Notifications.Token;
 import com.example.sunadchatapp.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,6 +22,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -67,8 +69,16 @@ public class ChatsFragment extends Fragment {
 
             }
         });
-
+        updateToken(FirebaseInstanceId.getInstance().getToken());
         return view;
+    }
+
+
+    private void updateToken(String token) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Tokens");
+        Token token1 = new Token(token);
+        reference.child(fuser.getUid()).setValue(token1);
+
     }
 
     private void chatList() {
