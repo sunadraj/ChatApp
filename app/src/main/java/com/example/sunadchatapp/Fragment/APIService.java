a8d4f682eb902bd4889da001d8d5e88134a014ba
sunad raj <40906488+sunadraj@users.noreply.github.com>
2020-06-25 16:16:24 +0530

    API service call through interface

---
+++ app/src/main/java/com/example/sunadchatapp/Fragment/APIService.java
@@ -0,0 +1,22 @@
+package com.example.sunadchatapp.Fragment;
+
+import com.example.sunadchatapp.Notifications.MyResponse;
+import com.example.sunadchatapp.Notifications.Sender;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @Headers(
+
+            {
+                    "Content-Type:application/json",
+                    "Authorization:keys=AAAA28Nix54:APA91bEClmrC_XK9wHfTuO6kR4ybkGSB8w5J1a_z1TggGjKbH0_tuKvvkIEZr-aC-wNMkp2ZJ5dI2um8jkO0VRCSkggD9zSGu1jdNWRItxkLZc2alt1Fzcg4fstypjMZMljyUEGpJMSR"
+            }
+    )
+
+    @POST("fcm/send")
+    Call<MyResponse> sendNotification(@Body Sender body);
+}
